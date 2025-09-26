7f05cc8a06ecd9b3fe44c680d5b7d265e799defb
strom314 <jakub.gros777@gmail.com>
2025-09-26 17:37:42 +0200

    create event listeners for the header buttons and make them switch the pages

--- src/index.js
+++ src/index.js
@@ -3,3 +3,26 @@ import { domManager as aboutPage } from "./about.js";
 import { domManager as menuPage } from "./menu.js";
 
 homePage.populateDom();
+
+const content = document.querySelector("#content");
+
+const homeButton = document.querySelector("#home");
+const menuButton = document.querySelector("#menu");
+const aboutButton = document.querySelector("#about");
+
+homeButton.addEventListener("click", loadHome);
+menuButton.addEventListener("click", loadMenu);
+aboutButton.addEventListener("click", loadAbout);
+
+function loadHome() {
+  content.innerHTML = "";
+  homePage.populateDom();
+}
+function loadMenu() {
+  content.innerHTML = "";
+  menuPage.populateDom();
+}
+function loadAbout() {
+  content.innerHTML = "";
+  aboutPage.populateDom();
+}
